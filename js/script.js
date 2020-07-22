af74c2238b0d742fe132ada08116ddce0c5f824b
souznikpasha <souznikpasha@gmail.com>
2020-07-22 19:49:16 +0700

    2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-(function($) { 
-    $(function() { 
-      $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
+$(document).ready(function(){
+
+  $('#menu-wrap').prepend("<div id='menu-icon'><b>Меню</b></div>");
+  $("#menu-icon").on("click", function(){
+    $(".menu").slideToggle();
+  });
   
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-  
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
-      });
-  
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-    
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-    }); 
-  })(jQuery); 
-  
+  });
