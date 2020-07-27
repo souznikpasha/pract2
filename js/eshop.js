91cdc4487f3de605ced865e2799202233c2e0fb9
souznikpasha <souznikpasha@gmail.com>
2020-07-27 12:11:04 +0700

    job js, cart 50%ready

--- js/eshop.js
+++ js/eshop.js
@@ -1,6 +1,10 @@
 var cart = {}; // корзина
 $('document').ready(function(){
     loadgoods();
+    showMiniCart();
+    checkCart();
+    
+    
 })
 
 function loadgoods(){
@@ -16,7 +20,7 @@ function loadgoods(){
                 out+='            <div class="middle"><img src="'+data[key].image+'" class="gallery"></div>';
                 out+='            <div class="bottom">';
                 out+='                <input type="button" data-art="'+key+'" value='+data[key]['cost']+' class="add-to-cart">';
-                out+='                <input type="submit" value="Удалить"  class="but-right">';
+                out+='                <input type="submit" data-art="'+key+'" value="Удалить"  class="but-right">';
                 out+='            </div>';
                 out+='        </div>';
                 out+='    </div>';
@@ -30,6 +34,7 @@ function loadgoods(){
             }*/
             $('#goods').html(out);
             $('input.add-to-cart').on('click', addToCart);
+            $('input.delete-to-cart').on('click', deletetocart);
         })
 }
 
@@ -42,5 +47,59 @@ function addToCart() {
     else {
     cart[articul] = 1;
     }
+    localStorage.setItem('cart', JSON.stringify(cart));
     console.log(cart);
+    showMiniCart();
+}
+
+function deletetocart(){
+    
+}
+
+function checkCart(){
+    //проверка наличие корзины в localstorage
+    if (localStorage.getItem('cart')!=null){
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+}
+
+
+
+function showMiniCart(){
+    //содержимое корзины
+    $.getJSON("goods.json", function (data){
+        var out =' ';
+        out +='<header class="header">';
+        out +='<img src="./images/logo.png" class="logo">';
+        out +='<p class="title">ShlyapKa</p>';
+            out +='<shop class="shop">';
+                
+                out +='<div id="main">';
+                    
+                    out +='   <div id="menu-wrap">'
+                    out +='        <ul class="menu">'
+                        out +='<li><a href=""><img src="./images/card.png" class="card" alt=""></a>';
+                            out +='<ul>';
+                            for (var key in cart){
+                                out +='<li>';
+                                    out +='<div id="main">';
+                                    out +='<div><img src="'+data[key].image+'" class="photosale"></div>';
+                                    //out +='<div><p>Шапка 3000р</p></div>';
+                                    out +='<div>'+data[key]['name']+' '+data[key]['cost']+' '+cart[key]+'шт</div>';
+                                    //out +='<div>'+cart[key]['articul']+' '+data[key]['cost']+'</div>';
+                                    out +='<div><a><img src="./images/Krest.png" class="photosale"></a></div>';
+                                    out +='</div>';
+                                out +='</li>';
+                                //out += i + ' - ' +cart[i]+'<br>';
+                            }
+                            out +='</ul>';
+                        out +='</li>';
+                    out +='</div>';
+                out +='<div class="textcard"><p>3</p></div>';
+                out +='<div class="textcard"><p>9000</p></div>';
+                out +='</div>';
+            out +='</shop>';
+        out +='</header>';
+        $('#mini-cart').html(out);
+    });
 }
